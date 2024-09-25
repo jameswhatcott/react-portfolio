ef000e44750f7cb0d286e27771d9607bfad0fd00
jameswhatcott <james@whatcott.com>
2024-09-24 20:00:51 -0500

    added the nav bar and basic set up for react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from './components/Nav';
+
+
+// Pass users array to the List component as a prop
+export default function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
