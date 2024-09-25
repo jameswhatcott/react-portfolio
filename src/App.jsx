ead60913ee9a9688271cbd7118d52565ca49f0ac
jameswhatcott <james@whatcott.com>
2024-09-24 20:26:27 -0500

    adjusted the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
 import AboutMe from './components/AboutMe';
+import ContactMe from './components/ContactMe';
+import Portfolio from './components/Portfolio';
 
 
 // Pass users array to the List component as a prop
 export default function App() {
   return (
+    <Router>
     <div>
       <Nav />
-      <AboutMe />
+      <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/contact" element={<ContactMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
     </div>
+    </Router>
   );
 }
