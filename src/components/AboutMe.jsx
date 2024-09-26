debb1bf8d4694a4a5097f680e41900eafbfcce70
jameswhatcott <james@whatcott.com>
2024-09-25 21:30:14 -0500

    adjusted the src for the photos

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
         <section id="about-me">
         <h2>About Me</h2>
         <div class="me">
-            <img id="me" src="../../Assets/headshot.jpeg" alt="headshot"/>
+            <img id="me" src="/Assets/headshot.jpeg" alt="headshot"/>
             <p>I'm an aspiring web developer based in Lufkin, Texas, passionate 
                 about creating valuable tools for companies and organizations. 
                 My journey into coding began with a digital marketing course, 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,15 +6,15 @@ export default function Portfolio() {
                 <h2>Portfolio</h2>
                 <div id="app-1">
                     <a href="#app-1"><h3>What's My Sky?</h3></a>
-                    <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src="../../Assets/constellation-weather pic.jpeg"/></a>
+                    <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src="/Assets/constellation-weather pic.jpeg"/></a>
                 </div>
                 <div id="app-2">
                     <a href="#app-2"><h3>Weather App</h3></a>
-                    <a href="https://jameswhatcott.github.io/weather-app/"><img src="../../Assets/weather app pic.png"/></a>
+                    <a href="https://jameswhatcott.github.io/weather-app/"><img src="/Assets/weather app pic.png"/></a>
                  </div>
                 <div id="app-3">
                     <a href="#app-3"><h3>Task Manager</h3></a>
-                    <a href="https://jameswhatcott.github.io/task-manager/"><img src="../../Assets/Task manager pic.png"/></a>
+                    <a href="https://jameswhatcott.github.io/task-manager/"><img src="/Assets/Task manager pic.png"/></a>
                  </div>
             </section>
 
