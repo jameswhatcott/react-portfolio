752f0c8a96f9acdb4564da7e42509bece2b13030
jameswhatcott <james@whatcott.com>
2024-10-09 15:08:14 -0500

    moved my files into the public folder

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,7 +8,7 @@ export default function Portfolio() {
             <h2>Portfolio</h2>
             <div id="app-1">
                 <a href="#app-1"><h3>What's My Sky?</h3></a>
-                <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src='/Assets/constellation weather pic.png' alt="Constellation Weather App"/></a>
+                <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src='/Assets/constellation-weather pic.png' alt="Constellation Weather App"/></a>
             </div>
             <div id="app-2">
                 <a href="#app-2"><h3>Weather App</h3></a>
