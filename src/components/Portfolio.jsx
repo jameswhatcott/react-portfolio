2edf70af39b4a7d487888a4cc0df07679e0d98b0
jameswhatcott <james@whatcott.com>
2025-02-20 21:21:47 -0600

    changed the headers on the portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,20 +6,20 @@ export default function Portfolio() {
     return (
         <section id="portfolio">
             <h2>Portfolio</h2>
-            <div id="app-0">
-                <a href="#app-0"><h3>Continue</h3></a>
+            <div id="app-0" class="app">
+                <h3>Continue</h3>
                 <a href="https://github.com/jameswhatcott/continue"><img src='/images/games page.png' alt="Continue"/></a>
             </div>
-            <div id="app-1">
-                <a href="#app-1"><h3>What's My Sky?</h3></a>
+            <div id="app-1" class="app">
+                <h3>What's My Sky?</h3>
                 <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src='/images/constellation-weather pic.jpeg' alt="Constellation Weather App"/></a>
             </div>
-            <div id="app-2">
-                <a href="#app-2"><h3>Weather App</h3></a>
+            <div id="app-2" class="app">
+                <h3>Weather App</h3>
                 <a href="https://jameswhatcott.github.io/weather-app/"><img src='/images/weather app pic.png' alt="Weather App"/></a>
             </div>
-            <div id="app-3">
-                <a href="#app-3"><h3>Task Manager</h3></a>
+            <div id="app-3" class="app">
+                <h3>Task Manager</h3>
                 <a href="https://jameswhatcott.github.io/task-manager/"><img src='/images/Task manager pic.png' alt="Task Manager"/></a>
             </div>
         </section>
