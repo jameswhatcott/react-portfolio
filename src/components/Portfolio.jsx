6056cec1699fa188c04cd31abb502170bde403db
jameswhatcott <162528300+jameswhatcott@users.noreply.github.com>
2025-06-24 14:03:13 -0500

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,19 +6,23 @@ export default function Portfolio() {
     return (
         <section id="portfolio">
             <h2>Portfolio</h2>
-            <div id="app-0" class="app">
+          <div id="app-0" class="app">
+                <h3>Todo API (Spring Boot)</h3>
+                <a href="https://todo-api-fy9m.onrender.com/"><img src='/images/todo-api.png' alt="todo-api"/></a>
+            </div>
+            <div id="app-1" class="app">
                 <h3>Continue</h3>
                 <a href="https://continue-4su3.onrender.com/"><img src='/images/games page.png' alt="Continue"/></a>
             </div>
-            <div id="app-1" class="app">
+            <div id="app-2" class="app">
                 <h3>What's My Sky?</h3>
                 <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src='/images/constellation-weather pic.jpeg' alt="Constellation Weather App"/></a>
             </div>
-            <div id="app-2" class="app">
+            <div id="app-3" class="app">
                 <h3>Weather App</h3>
                 <a href="https://jameswhatcott.github.io/weather-app/"><img src='/images/weather app pic.png' alt="Weather App"/></a>
             </div>
-            <div id="app-3" class="app">
+            <div id="app-4" class="app">
                 <h3>Task Manager</h3>
                 <a href="https://jameswhatcott.github.io/task-manager/"><img src='/images/Task manager pic.png' alt="Task Manager"/></a>
             </div>
