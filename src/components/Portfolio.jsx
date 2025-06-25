57106596f218547a118cc9f09492daa2b6f539fa
jameswhatcott <james@whatcott.com>
2025-06-24 21:35:44 -0500

    made some changes with a new photo

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,19 +7,23 @@ export default function Portfolio() {
         <section id="portfolio">
             <h2>Portfolio</h2>
             <div id="app-0">
-                <a href="#app-0"><h3>Continue</h3></a>
-                <a href="https://github.com/jameswhatcott/continue"><img src='/images/games page.png' alt="Continue"/></a>
+                <a href="#app-0"><h3>Todo API</h3></a>
+                <a href="https://todo-api-fy9m.onrender.com/"><img src='/images/todo-api.png' alt="todo-api"/></a>
             </div>
             <div id="app-1">
-                <a href="#app-1"><h3>What's My Sky?</h3></a>
-                <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src='/images/constellation-weather pic.jpeg' alt="Constellation Weather App"/></a>
+                <a href="#app-1"><h3>Continue</h3></a>
+                <a href="https://github.com/jameswhatcott/continue"><img src='/images/games page.png' alt="Continue"/></a>
             </div>
             <div id="app-2">
-                <a href="#app-2"><h3>Weather App</h3></a>
-                <a href="https://jameswhatcott.github.io/weather-app/"><img src='/images/weather app pic.png' alt="Weather App"/></a>
+                <a href="#app-2"><h3>What's My Sky?</h3></a>
+                <a href="https://jameswhatcott.github.io/constellation-weather-app/"><img src='/images/constellation-weather pic.jpeg' alt="Constellation Weather App"/></a>
             </div>
             <div id="app-3">
-                <a href="#app-3"><h3>Task Manager</h3></a>
+                <a href="#app-3"><h3>Weather App</h3></a>
+                <a href="https://jameswhatcott.github.io/weather-app/"><img src='/images/weather app pic.png' alt="Weather App"/></a>
+            </div>
+            <div id="app-4">
+                <a href="#app-4"><h3>Task Manager</h3></a>
                 <a href="https://jameswhatcott.github.io/task-manager/"><img src='/images/Task manager pic.png' alt="Task Manager"/></a>
             </div>
         </section>
